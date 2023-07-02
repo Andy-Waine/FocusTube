8a0c03d8b68424e6c968b0b0e031c1d400b01769
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-07-02 18:44:43 -0400

    updated target URLs for YouTube, added hide functionality for most of the main page

--- background.js
+++ background.js
@@ -1,23 +1,41 @@
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
-
-chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-        // Next state will always be the opposite of prevState
-        const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-
-        // Set the action badge to the next state
-        await chrome.action.setBadgeText({
-            tabId: tab.id,
-            text: nextState,
-        });
-    }
-})
-
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
-        text: 'OFF'
+      text: 'OFF'
     });
-});
+  });
+  
+  const landingPage = 'https://www.youtube.com/';
+  
+  // When the user clicks on the extension action
+  chrome.action.onClicked.addListener(async (tab) => {
+    if (tab.url.startsWith(landingPage)) {
+      // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+      // Next state will always be the opposite
+      const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  
+      // Set the action badge to the next state
+      await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState
+      });
+  
+      if (nextState === 'ON') {
+        // Insert the CSS file when the user turns the extension on
+        await chrome.scripting.insertCSS({
+          files: ['focus-mode.css'],
+          target: { tabId: tab.id }
+        });
+      } else if (nextState === 'OFF') {
+        // Remove the CSS file when the user turns the extension off
+        await chrome.scripting.removeCSS({
+          files: ['focus-mode.css'],
+          target: { tabId: tab.id }
+        });
+        // Reload the page to re-render the original CSS
+        await chrome.tabs.reload(tab.id);
+      }
+    }
+  });
+
+
