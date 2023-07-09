c077e9c7f716ecc38f673ed3123abd83cb499f67
Andy Waine <88730354+Andy-Waine@users.noreply.github.com>
2023-07-09 12:54:57 -0400

    further work on videoplayer page

--- background.js
+++ background.js
@@ -2,40 +2,42 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
       text: 'OFF'
     });
-  });
+});
   
-  const landingPage = 'https://www.youtube.com/';
-  
-  // When the user clicks on the extension action
-  chrome.action.onClicked.addListener(async (tab) => {
-    if (tab.url.startsWith(landingPage)) {
-      // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
-      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-      // Next state will always be the opposite
-      const nextState = prevState === 'ON' ? 'OFF' : 'ON';
-  
-      // Set the action badge to the next state
-      await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState
+const landingPage = 'https://www.youtube.com/';
+const searchPage = 'https://www.youtube.com/results?';
+
+// When the user clicks on the extension action
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(landingPage)) {
+    // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    // Next state will always be the opposite
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState
+    });
+
+    if (nextState === 'ON') {
+      // Insert the CSS file when the user turns the extension on
+      await chrome.scripting.insertCSS({
+        files: ['focus-mode.css'],
+        target: { tabId: tab.id }
       });
-  
-      if (nextState === 'ON') {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-          files: ['focus-mode.css'],
-          target: { tabId: tab.id }
-        });
-      } else if (nextState === 'OFF') {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-          files: ['focus-mode.css'],
-          target: { tabId: tab.id }
-        });
-        // Reload the page to re-render the original CSS
-        await chrome.tabs.reload(tab.id);
-      }
+    } else if (nextState === 'OFF') {
+      // Remove the CSS file when the user turns the extension off
+      await chrome.scripting.removeCSS({
+        files: ['focus-mode.css'],
+        target: { tabId: tab.id }
+      });
+      // Reload the page to re-render the original CSS
+      await chrome.tabs.reload(tab.id);
     }
-  });
+  } 
+});
+
 
 
